03033f16e1b078cb8426f2266c8f5e2e23bff149
7990 <bianchijarron@gmail.com>
2022-07-05 16:35:16 +0800

    建立父物件

---
+++ test_02/test_02/Models/SinoShape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_02.Models
+{
+    internal class SinoShape
+    {
+    }
+}
